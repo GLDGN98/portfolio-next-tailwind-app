ba786f406386db3d79c50dbb922aa0c5bcc79548
Gilad Dagan <giladagan53@gmail.com>
2023-08-08 13:15:05 +0300

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {}
 
 
 
-module.exports = nextConfig
+module.exports = {
+    // Your other configurations can go here
+  
+    // Add the "output" option for static HTML export
+    output: "export",
+  
+    // Add any other configurations you need
+  };
+  
