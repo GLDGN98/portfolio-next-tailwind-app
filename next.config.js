26a0e0a13cd3d08d5b822877b5870f16cc7180a5
Gilad Dagan <giladagan53@gmail.com>
2023-08-08 13:24:41 +0300

    added to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-
-
 module.exports = {
-    // Your other configurations can go here
-  
-    // Add the "output" option for static HTML export
-    output: "export",
-  
-    // Add any other configurations you need
-  };
-  
+  // Your other configurations can go here
+
+  // Add the "output" option for static HTML export
+  output: "export",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/public" : "",
+
+  // Add any other configurations you need
+}
